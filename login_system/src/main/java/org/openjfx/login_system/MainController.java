92900c65c1f31bb25cae4cc246bc4789e55c79bc
AJungersen <adam.jungersen@gmail.com>
2021-10-17 19:03:45 +0200

    Worked on database interaction with chat

--- login_system/src/main/java/org/openjfx/login_system/MainController.java
+++ login_system/src/main/java/org/openjfx/login_system/MainController.java
@@ -9,10 +9,12 @@ package org.openjfx.login_system;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.text.TextFlow;
+import static org.openjfx.login_system.App.db;
 
 /**
  * FXML Controller class
@@ -23,10 +25,15 @@ public class MainController {
 
     @FXML TextField chatBar;
     @FXML Button sendKnap;
-    @FXML TextFlow chatDisplay;
+    @FXML Label chatDisplay;
     
     String chatBarInput;
-
+    
+    //temporary variables. Delete before handing in
+    String currentSender = "Adam";
+    int currentChatRoom = 1;
+    
+        
     
     
     
@@ -42,11 +49,14 @@ public void textFieldEnter(KeyEvent e){
 }
 
     @FXML
-public void sendButton() throws IOException{
+public void sendButton() throws IOException, Exception{
     
     chatBarInput = chatBar.getText();
     System.out.println(chatBarInput);
+    Message mess = new Message(-1, chatBarInput, currentSender, currentChatRoom);
+    Database db = new Database();
     
+    db.savemessage(mess);
     
     
     chatBar.setText("");
